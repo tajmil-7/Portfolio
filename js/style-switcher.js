cd710315058241cde61632f5b8eaa6e1b0ba63dd
tajmil-7 <tajmilj@gmail.com>
2026-02-16 21:37:21 +0530

    changes

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,5 +1,24 @@
-/* =========================== toggle style switcher ==================================== */
+/* =========================== toggle style switcher ==================================== 
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
+})*/
+
+
+/* =========================== Theme Light and Dark Mode==================================== */
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
 })
+window.addEventListener("load",() =>{
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
+
